8f15f47f9703a074c2c92ef885b1db2c511be626
Nate Woods <big.nate.w@gmail.com>
2021-07-31 14:33:19 -0500

    messing around with the python logic

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -1,5 +1,4 @@
 import xarm
-import time
 
 try:
   arm = xarm.Controller('USB', debug=True)
@@ -8,38 +7,12 @@ except OSError:
 
 print('Battery voltage in volts:', arm.getBatteryVoltage())
 
-lose = [(1, 1500)]
-grab = [(1, 2200)]
-upright = [(2, 1500), (3, 1500), (4, 1500), (5, 1500)]
-pose1 = [(2, 1500), (3, 1000), (4, 1000), (5, 1970)]
-pivot1 = [(6, 1500)]
-pivot2 = [(6, 2000)]
+servos = [xarm.Servo(i, 1500) for i in range(1, 7)]
 
-def move(pose, duration=1000):
-  arm.setPosition(pose, duration=duration, wait=True)
-  time.sleep(1)
+def write(positions):
+    for i, p in enumerate(positions):
+        servos[i].position = int(p)
+    arm.setPosition(servos, duration=1, wait=True)
 
-"""
-move(lose)
-move(upright)
-move(pivot1)
-move(pose1, duration=3000)
-move(grab)
-move(upright, duration=3000)
-move(pivot2)
-move(pose1, duration=3000)
-move(lose)
-move(upright)
-move([(1, 2600)])
-move(lose)
-move(pose1, duration=3000)
-move(grab)
-move(upright, duration=3000)
-move(pivot1)
-move(pose1, duration=3000)
-move(lose)
-move(upright)
-"""
-
-import code
-code.interact(local=locals())
+def read():
+    pass
