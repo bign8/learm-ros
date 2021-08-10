9375c314428ba12e05e27715d466ec8d26385d7b
Nate Woods <big.nate.w@gmail.com>
2021-08-10 00:49:00 -0500

    idea about range specific casting

--- scripts/driver.py
+++ scripts/driver.py
@@ -27,6 +27,23 @@ servos = {
 }
 
 
+# TODO: use generic conversion function
+# https://www.arduino.cc/reference/en/language/functions/math/map/
+MODEL_RANGE = (-1.57, 1.57, float)
+ROBOT_RANGE = (  500, 2500, int)
+
+
+def convert(value, input_space, output_space):
+    """ https://www.arduino.cc/reference/en/language/functions/math/map/ """
+    assert input_space[0] <= value <= input_space[1], f"bad {input_space[0]} <= value <= {input_space[1]}"
+    in_min, in_max, in_type = input_space
+    out_min, out_max, out_type = output_space
+
+    # TODO: do smart type casting things here!
+
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
 def forward(x):
     """ Convert model inputs (radians) to servo PWM number (500-2500) """
     value = x.data # radians [-1.57, 1.57]
